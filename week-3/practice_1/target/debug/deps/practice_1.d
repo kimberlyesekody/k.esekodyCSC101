0bae8319e243776715faeff37fb6ac48bd63d2b3
kimberlyesekody <115807516+kimberlyesekody@users.noreply.github.com>
2022-11-18 20:33:37 +0100

    Delete week-3/practice_1/target directory

--- week-3/practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\kimbe\OneDrive\Documents\k.esekodyCSC101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\kimbe\OneDrive\Documents\k.esekodyCSC101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
